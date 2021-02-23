8ff01f9117248e10d71a6288ced1218fb45aa1d5
nimamadewen411 <1931205505@qq.com>
2021-02-23 10:04:01 +0800

    extract github config from code to properties

---
+++ src/main/java/com/myk/graduationdesign/dto/GitHubUser.java
@@ -0,0 +1,10 @@
+package com.myk.graduationdesign.dto;
+
+import lombok.Data;
+
+@Data
+public class GitHubUser {
+    private String name;
+    private Long id;
+    private String bio;
+}
---
+++ src/main/java/com/myk/graduationdesign/provider/GitHubProvider.java
@@ -0,0 +1,48 @@
+package com.myk.graduationdesign.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.myk.graduationdesign.dto.AccessTokenDTO;
+import com.myk.graduationdesign.dto.GitHubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GitHubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+        try (Response response = client.newCall(request).execute()){
+            String string = response.body().string();
+            System.out.println(string);
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(token);
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GitHubUser getUser(String accessTokenDTO){
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+ accessTokenDTO)
+                .build();
+        try (Response response = client.newCall(request).execute()){
+            String string = response.body().string();
+            GitHubUser gitHubUser = JSON.parseObject(string, GitHubUser.class);
+            return gitHubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
